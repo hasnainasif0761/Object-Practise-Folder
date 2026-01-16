2396ef296f544afdeafb5876250ffc83751d8a99
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-16 21:10:44 +0500

    Fourth-Updation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // var arr = ["a","b",true,2];
 // console.log(arr[arr.length-1]);
 
-// var studentData = [10,2,"male","ali"];
-// console.log(studentData[0])
+// // var studentData = [10,2,"male","ali"];
+// // console.log(studentData[0])
 
 
-// --- Objects ---- //
-// now every value have key (10 have called roll(value));
+// // --- Objects ---- //
+// // now every value have key (10 have called roll(value));
 
 // var studentData = {
 //     name:"Huzaifa",
@@ -15,7 +15,86 @@
 //     roll:10
 // }
 
-// console.table(studentData);
+
+// // var json = JSON.stringify(studentData)
+
+// // console.log(json);
+
+// var obj  = JSON.parse(studentData);
+
+// console.log(obj);
+
+
+// var student = {
+//     name : "ali",
+//     age: 10,
+//     subject: ["math","science","english"]
+// }
+
+// console.log(student.subject[2]);
+
+
+
+// var student = {
+//     name : "ali",
+//     age: 10,
+//     subject: ["math","science","english"],
+//     bio: function (){
+//         console.log("hello World");
+//     }
+// }
+
+// console.log(student.bio);
+
+
+
+
+// var student = {
+//     name : "ali",
+//     age: 10,
+//     subject: ["math","science","english"],
+//     bio: function (){
+//         console.log("hello World");
+//     },
+//     car: {
+//         name:"corola",
+//         color:'red',
+//         cc:760,
+//         fuelAverage: {low:200,hight:[200,300]}
+//     } 
+// }
+
+// console.log(student.car.fuelAverage.low);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
