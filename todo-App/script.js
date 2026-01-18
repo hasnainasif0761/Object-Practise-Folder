ea9f4f785112184b7dc9e8de00953ebc62d4c7b0
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-19 00:14:53 +0500

    14-Updation

--- todo-App/script.js
+++ todo-App/script.js
@@ -1,3 +1,38 @@
-var student = JSON.parse(localStorage.getItem('student')) || [];
+var students = JSON.parse(localStorage.getItem('students')) || [];
+
+if(document.getElementById('tbody')){
+    showstudent();
+}
+
+function addStudent(){
+    var name = document.getElementById('name').value;
+    var roll = document.getAnimations('roll').value;
+
+    if(name == "" || roll == ""){
+        alert('Please fill all Input Field');
+        return;
+    }
+
+    for(let i = 0; i < students.length;i++){
+        if(student[i].roll == roll){
+            alert('⚠️ Roll Number is Already exists');
+            return;
+        }
+    }
+
+    var student = {
+        name:name,
+        roll:roll
+    }
+
+    students.push(student);
+    localStorage.setItem('students',JSON.stringify(students));
+
+    alert('Student Submit Successfully 🎉🎉');
+
+    document.getElementById('name').value = "";
+    document.getElementById('roll').value = "";
+}
+
 
 
